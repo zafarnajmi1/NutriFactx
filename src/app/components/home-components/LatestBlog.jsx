09d18b8776409da95af750e504eecb7b9827d504
Zafar <zafarnajmi.mytechnology@gmail.com>
2026-09-20 19:05:16 +0500

    fix the website seo related issues

--- src/app/components/home-components/LatestBlog.jsx
+++ src/app/components/home-components/LatestBlog.jsx
@@ -2,7 +2,7 @@ import BlogsCard from "../common/BlogsCard";
 import { getMostViewedBlogs } from "@/lib/blogs";
 
 export default async function LatestBlogs() {
-  const latestPosts = await getMostViewedBlogs(10);
+  const latestPosts = await getMostViewedBlogs(8);
 
   if (!latestPosts.length) return null;
 
--- src/app/components/home-components/RecentPosts.jsx
+++ src/app/components/home-components/RecentPosts.jsx
@@ -2,7 +2,7 @@ import BlogsCard from "../common/BlogsCard";
 import { getRecentBlogs } from "@/lib/blogs";
 
 export default async function RecentPosts() {
-  const recentPosts = await getRecentBlogs(10);
+  const recentPosts = await getRecentBlogs(8);
 
   if (!recentPosts.length) return null;
 
